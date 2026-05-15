aa7c3fa1820aa703c38a0e1395f7e99fac415735
Vamsi Damodara <vamsidamodara82@gmail.com>
2026-05-15 10:32:14 +0530

    Upload screen Completed

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
 abstract class Routes {
   static const SPLASH = '/splash';
   static const PIN_SETUP = '/pin-setup';
   static const BIOMETRIC_SETUP = '/biometric-setup';
   static const PIN_LOCK = '/pin-lock';
   static const HOME = '/home';
-  
+  static const UPLOAD_DOCUMENT = '/upload-document';
+
 }
